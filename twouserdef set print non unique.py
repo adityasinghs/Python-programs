d627e10908ea277ba14f2e2dcfc94a87ea2f327c
Aditya Singh <161747228+adityasinghs@users.noreply.github.com>
2025-01-25 12:44:08 +0530

    Add files via upload
    Printing non unique numbers from two user defined set.

---
+++ twouserdef set print non unique.py
@@ -0,0 +1,59 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\thisi\Downloads\Python\q3.py ================
+Enter the size of element : 3
+Enter values : 1
+Enter values : 2
+Enter values : 3
+Enter the size of element of 2nd set : 1
+Enter values : 4
+{1, 2, 3, 4} set()
+
+================ RESTART: C:\Users\thisi\Downloads\Python\q3.py ================
+Enter the size of element : 3
+Enter values : 1
+Enter values : 2
+Enter values : 3
+Enter the size of element of 2nd set : 3
+Enter values : 1
+Enter values : 4
+Enter values : 5
+{1, 2, 3, 4, 5} {1}
+
+================ RESTART: C:\Users\thisi\Downloads\Python\q3.py ================
+Enter the size of element : 3
+Enter values : 1
+Enter values : 2
+Enter values : 3
+Enter the size of element of 2nd set : 3
+Enter values : 1
+Enter values : 2
+Enter values : 4
+{1, 2, 3, 4} {1, 2}
+
+================ RESTART: C:\Users\thisi\Downloads\Python\q3.py ================
+Enter the size of element : 3
+Enter values : 1
+Enter values : 2
+Enter values : 3
+Enter the size of element of 2nd set : 3
+Enter values : 1
+Enter values : 5
+Enter values : 9
+{9, 2, 3, 5}
+>>> 
+================ RESTART: C:\Users\thisi\Downloads\Python\q3.py ================
+Enter the size of element : 5
+Enter values : 1
+Enter values : 2
+Enter values : 5
+Enter values : 8
+Enter values : 6
+Enter the size of element of 2nd set : 5
+Enter values : 1
+Enter values : 7
+Enter values : 8
+Enter values : 5
+Enter values : 9
+Non-unique :  {8, 1, 5}
